c5256e06bf70999454324b1bbc6ba77f1b744a80
archig97 <archismitaghosh@yahoo.com>
2024-11-25 21:01:14 -0500

    made fixes to search feature

--- search/urls.py
+++ search/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import city_suggestions, city_photo
-from . import views
+from .views import city_suggestions, city_photo, locate_me
+
 urlpatterns = [
     path("city", city_suggestions, name="city_search"),  # Returns city suggestions based on user input.
     path("city/photo", city_photo, name="city_photo") , # Fetches a photo of the specified city.
-    path('locate-me/', views.locate_me, name='locate_me')
+    path('locate-me/', locate_me, name='locate_me')
 ]
